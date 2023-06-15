dd0e28c1c0d26e29e986158169190dc242dd9b84
apxlin <apxlin2018@gmail.com>
2023-06-14 23:01:51 -0700

    6.14

--- velox/optimizer/OptimizerTest.cpp
+++ velox/optimizer/OptimizerTest.cpp
@@ -12,8 +12,10 @@
 #include "velox/exec/tests/utils/PlanBuilder.h"
 #include "velox/vector/tests/utils/VectorMaker.h"
 #include "velox/optimizer/Optimizer.h"
+#include "velox/exec/tests/utils/AssertQueryBuilder.h"
 
 using namespace facebook::velox;
+using namespace facebook::velox::exec::test;
 using namespace facebook::velox::test;
 using namespace facebook::velox::optimizer;
 
@@ -64,6 +66,53 @@ public:
 
 };
 
+class VectorPlus: public exec::VectorFunction {
+public:
+    VectorPlus() {
+    }
+
+     
+    FlatVectorPtr<int64_t> vec_1;
+    FlatVectorPtr<int64_t> vec_2;
+
+
+    void apply(
+        const SelectivityVector& rows,
+        std::vector<VectorPtr>& args,
+        const TypePtr& type,
+        exec::EvalCtx& context,
+        VectorPtr& output) const override {
+
+        auto arg1 = args[0]->as<FlatVector<int64_t>>();
+        auto arg2 = args[1]->as<FlatVector<int64_t>>();
+        auto size = arg1->size();
+        auto result = BaseVector::create<FlatVector<int64_t>>(type, size, context.pool());
+        
+        
+        for (auto i = 0; i < size; ++i) {
+            result->set(i, arg1->valueAt(i) * arg2->valueAt(i));
+        }
+        output = result;
+    }
+
+    static std::vector<std::shared_ptr<exec::FunctionSignature>> signatures() {
+        return {exec::FunctionSignatureBuilder()
+                     .returnType("BIGINT")
+                     .argumentType("BIGINT")
+                     .argumentType("BIGINT")
+                     .build()};
+
+    }
+
+    int getSize() const override {
+      return 0;
+    }
+    static exec::VectorFunctionMetadata metadata() {
+    return {true /* supportsFlattening */};
+  }
+
+};
+
 
 auto pool_ = memory::addDefaultLeafMemoryPool();
 std::shared_ptr<folly::Executor> executor_{
@@ -90,23 +139,105 @@ int main(int argc, char** argv) {
         std::make_unique<AddVectorToConstant>(myVec, 5),
         AddVectorToConstant::metadata());
 
-  auto col1 = maker.flatVector({0, 1, 2, 3, 4});
-  auto col2 = maker.flatVector({1, 2, 3, 4, 5});
-  auto inputRowVector = maker.rowVector({"col1", "col2"}, {col1, col2});
+  exec::registerVectorFunction(
+        "vec_plus",
+        VectorPlus::signatures(),
+        std::make_unique<VectorPlus>(),
+        VectorPlus::metadata());
+  // auto col1 = maker.flatVector({0, 1, 2, 3, 4});
+  // auto col2 = maker.flatVector({1, 2, 3, 4, 5});
+  // auto inputRowVector = maker.rowVector({"col1", "col2"}, {col1, col2});
+
+  auto row = maker.flatVector({0, 0, 1, 1});
+  auto col = maker.flatVector({0, 1, 0, 1});
+  auto va = maker.flatVector({1, 2, 3, 4});
+  auto vb = maker.flatVector({11, 12, 13, 14});
+  // {1,2 plus {11,12
+  //  3,4}      13,14}
+  auto inputRowVectorJoinA = maker.rowVector({"a_row", "a_col", "a_value"}, {row, col, va});
+  auto inputRowVectorJoinB = maker.rowVector({"b_row", "b_col", "b_value"}, {row, col, vb});
+
+  auto inputRowVectorJoin = maker.rowVector({"a_row", "a_col", "a_value", "b_row", "b_col", "b_value"}, {row, col, va, row, col, vb});
+  auto JoinT = maker.rowVector({"table_a", "table_b"},{inputRowVectorJoinA, inputRowVectorJoinB});
+  //two tables means nestjoin or other format of plan
 
   auto myPlan = exec::test::PlanBuilder()
-                  .values({inputRowVector})
-                  .filter("col1 > 2")
-                  .project({"vec_add_to_constant(col1)"})
+                  .values({JoinT})
+                  .project({"vec_plus(table_a.a_value, table_b.b_value) AS result"})
 		            .planFragment();
 
-  facebook::velox::optimizer::Optimizer op(queryCtx_);
-  op.traverse(myPlan);
+  // auto task1 = std::make_shared<exec::Task>("task1", myPlan, 0, queryCtx_);
+  // // Execute the plan above
+  // auto result1 = task1->next();
+  // std::cout << "Results for Query 1:" << result1->toString() << std::endl;
+  // std::cout << result1->toString(0, result1->size()) << std::endl;
+  core::PlanNodeId Id;
+  auto myPlan2 = exec::test::PlanBuilder()
+                  .values({JoinT})
+                  .project({"vec_plus(table_a.a_value, table_b.b_value) AS result"})
+		            .planNode();
+
+  auto planNodeIdGenerator = std::make_shared<core::PlanNodeIdGenerator>();
+  core::PlanNodeId AId;
+  core::PlanNodeId BId;
+  auto planjoin = exec::test::PlanBuilder(planNodeIdGenerator)
+            .values({JoinT})
+            .project({"table_a.a_col", "table_a.a_row","table_a.a_value"})
+            .capturePlanNodeId(AId)
+            .hashJoin(
+                {"a_col"},
+                {"b_row"},
+                exec::test::PlanBuilder(planNodeIdGenerator)
+                    .values({JoinT})
+                    .project({"table_b.b_col", "table_b.b_row","table_b.b_value"})
+                    .capturePlanNodeId(BId)
+                    .planNode(),
+                "", // extra filter
+                {"a_row","b_col", "a_value", "b_value"})
+            .project({"a_row", "b_col","a_value * b_value AS mp"})
+            .singleAggregation({"a_row","b_col"}, {"sum(mp) AS result"})
+            .project({"result"})
+            .planNode();
+
+  // auto taskj = std::make_shared<exec::Task>("taskj", planjoin, 0, queryCtx_);
+  // auto res = taskj->next();
+  // std::cout << "Results for Query 1:" << res->toString() << std::endl;
+  // std::cout << res->toString(0, res->size()) << std::endl;
+auto oldplan = myPlan2->sources()[0];
+auto source = planjoin->sources()[0]->sources()[0]->sources()[0]->sources()[0]->sources()[0];
+source = oldplan;
+
+  auto res2 = AssertQueryBuilder(myPlan2).copyResults(pool_.get());
+  std::cout << "Results for Query 3:" << res2->toString() << std::endl;
+  std::cout << res2->toString(0, res2->size()) << std::endl;
 
 
-  auto task3 = std::make_shared<exec::Task>("task3", myPlan, 0, queryCtx_);
-  // Execute the plan above
-  auto result3 = task3->next();
-  std::cout << "Results for Query 1:" << result3->toString() << std::endl;
-  std::cout << result3->toString(0, result3->size()) << std::endl;
+  auto res = AssertQueryBuilder(planjoin).copyResults(pool_.get());
+  std::cout << "Results for Query 2:" << res->toString() << std::endl;
+  std::cout << res->toString(0, res->size()) << std::endl;
+
+  // auto newplan = std::shared_ptr<const PlanNode>;
+
+  // newplan->sources()[0] = myPlan2->sources()[0];
+
+  // planjoin->sources()[0]->sources()[0]->sources()[0]->sources()[0]->sources()[0] = const_cast<std::shared_ptr<const facebook::velox::core::PlanNode>&>(myPlan2->sources()[0]);
+  // auto newplan = planjoin;
+
+  // auto res = AssertQueryBuilder(planjoin).copyResults(pool_.get());
+  // std::cout << "Results for Query 2:" << res->toString() << std::endl;
+  // std::cout << res->toString(0, res->size()) << std::endl;
+  // myPlan2->sources()[0] = planjoin->sources()[0];
+
+  // facebook::velox::optimizer::Optimizer op(queryCtx_);
+  // auto optimizedPlan = op.op(myPlan);
+  // op.traverse(myPlan);
+
+  // auto res = AssertQueryBuilder(optimizedPlan).copyResults(pool_.get());
+  // std::cout << "Results for Query 3:" << res->toString() << std::endl;
+  // std::cout << res->toString(0, res->size()) << std::endl;
+
+
+
+  // std::cout << "Results for Query:" << results->toString() << std::endl;
+  // std::cout << results->toString(0, results->size()) << std::endl;
 }
