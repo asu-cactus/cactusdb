c44fd337470a282c32011a6e9ec6a8ab3b3dab95
apxlin <apxlin2018@gmail.com>
2023-09-01 18:22:28 -0700

    9.1

--- velox/optimizer/OptimizerTest.cpp
+++ velox/optimizer/OptimizerTest.cpp
@@ -1659,10 +1659,10 @@ void test_mnist_oom_error(int argc, char** argv){
   int num_splits = 1;
   for(int i=0; i < num_splits; i++)
     paths.push_back(file);
-  auto hiveSplits = myfile.makeHiveConnectorSplits(paths);
-  // auto hiveSplits =  myfile.makeHiveConnectorSplits(file->path, 4, dwio::common::FileFormat::DWRF);
+  // auto hiveSplits = myfile.makeHiveConnectorSplits(paths);
+  auto hiveSplits =  myfile.makeHiveConnectorSplits(file->path, 4, dwio::common::FileFormat::DWRF);
  
-  int concurrency = 8;
+  int concurrency = 4;
   boost::interprocess::interprocess_semaphore semaphore(concurrency);
 
   auto task = exec::Task::create("0", plan0, 0, queryCtx_, 
@@ -1970,7 +1970,7 @@ void test_oom_success(int argc, char** argv){
         return exec::BlockingReason::kNotBlocked;
   });
 
-  task->start(task, 4);
+  task->start(task, 1);
   std::cout << "Hive splits:" << std::endl;
   for(auto& split : hiveSplits) {
     semaphore.wait();
@@ -2064,13 +2064,460 @@ void test_oom_success(int argc, char** argv){
   // std::cout << "Total time (sec) = " <<  (std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()) /1000000.0 << std::endl;
   
 }
+void data_loader(){
+
+    VectorMaker maker{pool_.get()};
+    int input_size = 597540; // num_features
+    int layer1_size = 1024; // num units in hidden layer 1
+    int layer2_size = 14588; // cat
+    int num_samples = 1000;
+
+    std::ifstream weights_file("/home/local/ASUAD/qlin36/w1024.txt"); 
+    std::ifstream bias_file("/home/local/ASUAD/qlin36/b1024.txt"); 
+    std::ifstream test_file("/home/local/ASUAD/qlin36/x_test_large.txt"); 
 
 
+    FlatVectorPtr<float> weights_1 = get_tensor(maker, weights_file, layer1_size * input_size, input_size);
+    FlatVectorPtr<float> bias_1 = get_tensor(maker, bias_file, layer1_size, 1);
+    FlatVectorPtr<float> weights_2 = get_tensor(maker, weights_file, layer2_size * layer1_size, layer1_size);
+    FlatVectorPtr<float> bias_2 = get_tensor(maker, bias_file, layer2_size, 1);
+    weights_file.close();
+    bias_file.close();
+
+    float* bias_1_values = bias_1->values()->asMutable<float>();
+    float* bias_2_values = bias_2->values()->asMutable<float>();
+
+    FlatVectorPtr<float> bias_1_mat = maker.flatVector<float>(num_samples * layer1_size);
+    for(int i=0; i < bias_1_mat->size(); i++)
+      bias_1_mat->set(i, bias_1_values[i%layer1_size]);
+    
+    FlatVectorPtr<float> bias_2_mat = maker.flatVector<float>(num_samples * layer2_size);
+    for(int i=0; i < bias_2_mat->size(); i++)
+      bias_2_mat->set(i, bias_2_values[i%layer2_size]);
+
+    FlatVectorPtr<float> input = get_tensor(maker, test_file, input_size * num_samples, num_samples);
+    float* data = input->values()->asMutable<float>();
+}
+
+std::vector<std::vector<float>> create_input_block(int total_size, std::vector<std::vector<float>>& values, int block_numbers){
+    std::vector<std::vector<float>> valuesArray;
+    std::vector<float> flattened;
+    for (const auto& row : values) {
+        flattened.insert(flattened.end(), row.begin(), row.end());
+    }
+
+    auto block_size = total_size / block_numbers;
+    for (int i = 0; i < block_numbers; i++) {
+        std::vector<float> valuesArraySingleBlock;
+        for (int j = 0; j < block_size; j++) {
+                valuesArraySingleBlock.push_back(flattened[i*block_size+j]);
+        }
+        valuesArray.push_back(valuesArraySingleBlock);
+    }
+    return valuesArray;
+}
+
+std::vector<std::vector<float>> create_weight_block(int total_size, float* values, int block_numbers){
+    std::vector<std::vector<float>> valuesArray;
+    auto block_size = total_size / block_numbers;
+    for (int i = 0; i < block_numbers; i++) {
+        std::vector<float> valuesArraySingleBlock;
+        for (int j = 0; j < block_size; j++) {
+                valuesArraySingleBlock.push_back(values[i*block_size+j]);
+        }
+        valuesArray.push_back(valuesArraySingleBlock);
+    }
+    return valuesArray;
+}
+
+std::vector<std::vector<float>> create_block_index(int parts, int flag){
+  std::vector<std::vector<float>> indexs;
+  if (flag == 0){
+    std::vector<float> indexs_row;
+    std::vector<float> indexs_col;
+    for (int i = 0; i < parts; i++){
+      indexs_row.push_back(0);
+    }
+    for (int i = 0; i < parts; i++){
+      indexs_col.push_back(i);
+    }
+    indexs.push_back(indexs_row);
+    indexs.push_back(indexs_col);
+  }
+  else {
+    std::vector<float> indexs_row;
+    std::vector<float> indexs_col;
+    for (int i = 0; i < parts; i++){
+      indexs_row.push_back(i);
+    }
+    for (int i = 0; i < parts; i++){
+      indexs_col.push_back(0);
+    }
+    indexs.push_back(indexs_row);
+    indexs.push_back(indexs_col);
+  }
+  return indexs;
+}
+
+struct FileStructure {
+  std::vector<std::shared_ptr<TempFilePath>> paths;
+  RowTypePtr schema;
+}
+
+FileStructure block_to_files(std::vector<std::vector<float>> valuesArray, int parts, int flag){
+  MyFileTest myFile;
+  FileStructure myFileStructure;
+  std::vector<std::shared_ptr<TempFilePath>> paths;
+  if (flag == 0){
+    auto indexs = create_block_index(parts, flag);
+    for (int i = 0; i < parts; i++){
+      auto input = maker.rowVector({"v", "v_row", "v_col"}, 
+      {maker.arrayVector<float>({valuesArray[i]}, REAL()), maker.flatVector({indexs[0][i]}), maker.flatVector({indexs[1][i]})});
+      auto file = TempFilePath::create();
+      myFile.writeToFile(file->path, {input});
+      paths.push_back(file);
+  }
+  myFileStructure.paths = paths;
+  myFileStructure.schema = asRowType(input->type());
+  return myFileStructure;
+  }
+  else {
+    auto indexs = create_block_index(parts, flag);
+    for (int i = 0; i < parts; i++){
+      auto input = maker.rowVector({"w", "w_row", "w_col"}, 
+      {maker.arrayVector<float>({valuesArray[i]}, REAL()), maker.flatVector({indexs[0][i]}), maker.flatVector({indexs[1][i]})});
+      auto file = TempFilePath::create();
+      myFile.writeToFile(file->path, {input});
+      paths.push_back(file);
+  }
+  myFileStructure.paths = paths;
+  myFileStructure.schema = asRowType(input->type());
+  return myFileStructure;
+  }
+
+}
+
+struct DataFrame {
+    std::vector<std::vector<float>> features;
+    std::vector<float*> weights;
+    std::vector<float*> bias;
+};
+
+DataFrame data_generate(int features, int samples, int first_layer, int second_layer){
+  int input_features_size = features;
+  int num_samples = samples;
+
+  int first_layer_output_size = first_layer;
+  int second_layer_output_size = second_layer;
+  // ( 1000 * 597540 x 597540 * 1024 + 1000*1024) first layer
+  // ( 1000 * 1024 x 1024 * 14588 + 1000*14588) second layer
+  int input_total_size = input_features_size * num_samples;
+
+  int weight_layer1_size = input_features_size * first_layer_output_size;
+  int weight_layer2_size = first_layer_output_size * second_layer_output_size;
+
+  int bias_layer1_size = num_samples * first_layer_output_size;
+  int bias_layer2_size = num_samples * second_layer_output_size;
+
+  //generate input
+  std::vector<std::vector<float>> featureVectors;
+  for (int i = 0; i < num_samples; i++) {
+        std::vector<float> featureVector;
+        for (int j = 0; j < input_features_size; j++) {
+                featureVector.push_back(i*input_features_size+j);
+        }
+        featureVectors.push_back(featureVector);
+    }
+
+  //generate weight
+  float* weight_layer1 = new float[weight_layer1_size];
+  for (int i = 0; i < weight_layer1_size; ++i) {
+      weight_layer1[i] = i; 
+  }
+  float* weight_layer2 = new float[weight_layer2_size];
+  for (int i = 0; i < weight_layer2_size; ++i) {
+      weight_layer2[i] = i; 
+  }
+  std::vector<float*> weights;
+  weights.push_back(weight_layer1);
+  weights.push_back(weight_layer2);
+
+  //generate bias
+  float* bias_layer1 = new float[bias_layer1_size];
+  for (int i = 0; i < bias_layer1_size; ++i) {
+      bias_layer1[i] = i; 
+  }
+  float* bias_layer2 = new float[bias_layer2_size];
+  for (int i = 0; i < weight_layer2_size; ++i) {
+      bias_layer2[i] = i; 
+  }
+  std::vector<float*> bias;
+  bias.push_back(bias_layer1);
+  bias.push_back(bias_layer2);
+  // create dataframe
+  DataFrame data;
+  data.features = featureVectors;
+  data.weights = weights;
+  data.bias = bias;
+
+  return data;
+}
+
+PlanBuilder& build_plan_udf(DataFrame data, int features, int first_layer, int second_layer){
+  
+  auto featureArrayVector = maker.arrayVector<float>(data.features, REAL());
+  auto inputRowVector = maker.rowVector({"v"}, {featureArrayVector});
+  std::string compute =  NNBuilder()
+                        .denseLayer(first_layer, features, data.weights[0], data.bias[0], NNBuilder::RELU)
+                        .denseLayer(second_layer, first_layer, data.weights[1], data.bias[1], NNBuilder::SOFTMAX)
+                        .build();
+
+
+  std::cout << compute << std::endl; // softmax5(mat_add4(mat_mul3(relu2(mat_add1(mat_mul0({}))))))
+
+  auto planBuilder = exec::test::PlanBuilder(pool_.get())
+                .tableScan(asRowType(inputRowVector->type()))
+                .capturePlanNodeId(p0)
+                .project({fmt::format(compute, "v")}) 
+                .planBuild();
+
+  return planBuilder;
+}
+
+void exec_plan_udf(PlanBuilder& planBuilder, int memoryLimit, std::vector<std::vector<float>> features, int splitsNum, int threadsNum){
+  std::shared_ptr<memory::MemoryPool> rootPool{memory::defaultMemoryManager().addRootPool("root", memoryLimit * MB)};
+  auto planFragment = planBuilder.planFragment();
+  queryCtx_->testingOverrideMemoryPool(rootPool);
+  queryCtx_->testingOverrideConfigUnsafe({{core::QueryConfig::kSpillEnabled, "false"}});
+  
+  auto featureArrayVector = maker.arrayVector<float>(features, REAL());
+  auto inputRowVector = maker.rowVector({"v"}, {featureArrayVector});
+  auto file = TempFilePath::create();
+  MyFileTest myFile;
+  myFile.writeToFile(file->path, {inputRowVector});
+
+  auto hiveSplits =  myFile.makeHiveConnectorSplits({file->path}, splitsNum, dwio::common::FileFormat::DWRF);
+
+  boost::interprocess::interprocess_semaphore semaphore(threadsNum);
+
+  auto task = exec::Task::create("0", planFragment, 0, queryCtx_, 
+        [&semaphore](RowVectorPtr result, ContinueFuture* /*unused*/) {
+          if(result){
+            semaphore.post();
+          }
+          return exec::BlockingReason::kNotBlocked;
+  });
+
+  task->start(task, threadsNum);
+  std::cout << "Hive splits:" << std::endl;
+  for(auto& split : hiveSplits) {
+    semaphore.wait();
+    std::cout << split->toString() << std::endl;
+    task->addSplit(p0, exec::Split(std::move(split)));
+  }
+  task->noMoreSplits(p0);
+  std::cout << std::endl;
+ 
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+  waitForFinishedDrivers(task);
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+  std::cout << "Total time (sec) = " <<  (std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()) /1000000.0 << std::endl;
+
+  // another execute method
+  auto planAssert = planBuilder.planNode();
+  DuckDbQueryRunner duckDbQueryRunner_;
+  auto results = exec::test::AssertQueryBuilder(planAssert, duckDbQueryRunner_)
+  .splits(p0, hiveSplits)
+  .copyResults(pool_.get());
+  std::cout << "test udf Results:" << results->toString() << std::endl;
+  std::cout << results->toString(0, results->size()) << std::endl;
+}
+
+struct DynamicMetaData {
+    bool replaceFlag;
+    int blocksNum;
+    std::vector<int> leftBlockSize;
+    std::vector<int> rightBlockSize;
+    std::vector<int> planIdShot;
+    std::vector<std::string> targetStr;
+};
+
+struct OptOutput {
+  bool flag;
+
+  std::vector<std::shared_ptr<TempFilePath>> inputsPaths;
+  std::vector<std::shared_ptr<TempFilePath>> weightPaths;
+  PlanBuilder& planBuilerOpt;
+}
+
+DynamicMetaData decision_maker(PlanBuilder& planBuilder){
+  //Todo: add true dynamic tool
+    DynamicMetaData decisions;
+    decisions.replaceFlag = true;
+    decisions.blocksNum = 4;
+    decisions.planIdShot = {0, 1, 0, 0, 1};
+    decisions.leftBlockSize.push_back(1000);
+    decisions.leftBlockSize.push_back(149385);
+    decisions.rightBlockSize.push_back(149385);
+    decisions.rightBlockSize.push_back(1024);
+    decisions.targetStr.push_back("mat_mul0(v)");
+    return decisions;
+}
+
+PlanBuilder& build_plan_op(float* weight, int row, int col, RowTypePtr inputs, RowTypePtr weights, std::vector<std::string> str){
+  exec::registerVectorFunction(
+    "mat_mul_b",
+    MatrixMultiply_b::signatures(),
+    std::make_unique<MatrixMultiply_b>(row, col, weight)
+  );
+  
+  std::string searchString = str;
+  std::string replaceString = "result";
+
+  std::size_t found = str[0].find(searchString);
+  while (found != std::string::npos) {
+      str[0].replace(found, searchString.length(), replaceString);
+      found = str[0].find(searchString, found + replaceString.length());
+  }
+  
+  auto planNodeIdGenerator = std::make_shared<core::PlanNodeIdGenerator>();
+  core::PlanNodeId p2;
+  core::PlanNodeId p3;
+  auto planOpt = exec::test::PlanBuilder(planNodeIdGenerator)
+                  .tableScan(inputs)
+                  // .values({input})
+                  .capturePlanNodeId(p2)
+                  .hashJoin(
+                      {"v_col"},
+                      {"w_row"},
+                    exec::test::PlanBuilder(planNodeIdGenerator)
+                   .tableScan(weights)
+                  // .values({weightb})
+                   .capturePlanNodeId(p3)
+                   .planNode(),
+                    "", // extra filter
+                    {"v_row", "w_col", "v", "w"})
+                  .project({"v_row", "w_col", "mat_mul_b(v, w) AS mp"})
+                  .singleAggregation({"w_col","v_row"}, {"array_sum(mp) AS result"})
+                  .project({str[0]})
+                  .planBuild();
+  return planOpt;
+}
+
+OptOutput optiming_plan(PlanBuilder& planBuilder, DataFrame data, int num_samples, int features_size, int first_layer_size, int second_layer_size){
+  auto dyDecision = decision_maker(planBuilder);
+  OptOutput output;
+  if (dyDecision.replaceFlag) {
+    auto inputBlocks = create_input_block(num_samples*features_size, data.features, dyDecision.blocksNum);
+    //here is only for layer 1
+    auto weightBlocks = create_weight_block(features_size*first_layer_size, data.weights[0], dyDecision.blocksNum);
+    auto inputs = block_to_files(inputBlocks, blocksNum, 0);//0 denote values, 1 denote weight
+    auto weights = block_to_files(weightBlocks, blocksNum, 1);
+    auto planBuilerOpt = build_plan_op(data.weights[0], dyDecision.rightBlockSize[0], dyDecision.rightBlockSize[1], inputs.schema, weights.schema, decisions.targetStr);
+    output.flag = true;
+    output.inputsPaths = inputs.paths;
+    output.weightPaths = weights.paths;
+    output.planBuilderOpt = planBuilerOpt;
+    return output;
+  }
+  else {
+    output.flag = false;
+    output.planBuilderOpt = planBuilder;
+    return output;
+  }
+}
+
+
+
+void exec_plan_relational(PlanBuilder& planBuilderOpt, int memoryLimit, std::vector<std::shared_ptr<TempFilePath>> inputPaths, 
+std::vector<std::shared_ptr<TempFilePath>> weightPaths, int threadsNum){
+  std::shared_ptr<memory::MemoryPool> rootPool{memory::defaultMemoryManager().addRootPool("root", memoryLimit * MB)}; // 280 pass for 4 threads, 40 for 1 thread
+  queryCtx_->testingOverrideMemoryPool(rootPool);
+  auto planFragmentOpt = planBuilderOpt.planFragment();
+  MyFileTest myFile;
+  auto inputHiveSplits = myFile.makeHiveConnectorSplits(inputPaths);
+  auto weightHiveSplits = myFile.makeHiveConnectorSplits(weightPaths);
+
+  boost::interprocess::interprocess_semaphore semaphore(threadsNum*2);
+  auto task = exec::Task::create("0", planFragmentOpt, 0, queryCtx_, 
+      [&semaphore](RowVectorPtr result, ContinueFuture* /*unused*/) {
+        if(result){
+          semaphore.post();
+        }
+        return exec::BlockingReason::kNotBlocked;
+  });
+
+  task->start(task, threadsNum);
+  std::cout << "Hive splits:" << std::endl;
+  for(auto& split : inputHiveSplits) {
+    semaphore.wait();
+    std::cout << split->toString() << std::endl;
+    task->addSplit(p2, exec::Split(std::move(split)));
+  }
+  for(auto& split : weightHiveSplits) {
+    semaphore.wait();
+    std::cout << split->toString() << std::endl;
+    task->addSplit(p3, exec::Split(std::move(split)));
+  }
+  task->noMoreSplits(p2);
+  task->noMoreSplits(p3);
+
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+  waitForFinishedDrivers(task);
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+  std::cout << "Total time (sec) = " <<  (std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()) /1000000.0 << std::endl;
+
+  //anthor execute method
+  auto planOptAssert = planBuilderOpt.planNode();
+  DuckDbQueryRunner duckDbQueryRunner_;
+  auto results = exec::test::AssertQueryBuilder(planOptAssert, duckDbQueryRunner_)
+  .splits(p2, myFile.makeHiveConnectorSplits(inputHiveSplits))
+  .splits(p3, myFile.makeHiveConnectorSplits(weightHiveSplits))
+  .copyResults(pool_.get());
+  std::cout << "a1 values Results:" << results->toString() << std::endl;
+  std::cout << results->toString(0, results->size()) << std::endl;
+
+}
+
+void test_optimizer_demo(argc, argv){
+
+  folly::init(&argc, &argv, false);
+  functions::prestosql::registerAllScalarFunctions();
+  aggregate::prestosql::registerAllAggregateFunctions();
+  parse::registerTypeResolver();
+  
+
+
+
+  int input_features_size = 597540;
+  int num_samples = 1000;
+
+  int first_layer_output_size = 1024;
+  int second_layer_output_size = 14588;
+
+  int memory_limit_udf = 38;//mb
+  int splits_num_udf = 4;
+  int threads_num_udf = 4;
+
+  int memory_limit_rela = 102;//mb
+  int splits_num_rela = 4;
+  int threads_num_rela = 4;
+  
+  auto data = data_generate(input_features_size, num_samples, first_layer_output_size, second_layer_output_size);
+  auto udf_plan_builder = build_plan_udf(data, input_features_size, first_layer_output_size, second_layer_output_size);
+  exec_plan_udf(udf_plan_builder, memory_limit_udf, data.features, splits_num_udf, threads_num_udf);
+  
+  auto relational_plan = optiming_plan(udf_plan_builder, data, num_samples, input_features_size, first_layer_output_size, second_layer_output_size);
+  exec_plan_relational(relational_plan.planBuilderOpt, memory_limit_rela, relational_plan.inputsPaths, relational_plan.weightPaths, threads_num_rela);
+}
+
+VectorMaker maker{pool_.get()};
 int main(int argc, char** argv) {
     // test_optimizer(argc, argv);
     // test_mnist_optimizer(argc, argv, 1);
     // test_mnist_oom_error(argc, argv);
-    test_oom_success(argc, argv);
+    // test_oom_success(argc, argv);
+    test_optimizer_demo(argc, argv);
 
 
 
