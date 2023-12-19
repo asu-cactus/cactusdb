50535d8d7a1c8ed45b1ad61ec3c975f50cc18a4b
lixi-zhou <lixi.zhou@asu.edu>
2023-12-19 15:34:53 +0000

    refactor code

---
+++ db-ml/baseline/load_data_to_db.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+import psycopg2
+import pandas as pd
+import utils
+from sqlalchemy import create_engine
+import argparse
+from sklearn.preprocessing import LabelEncoder
+
+def load_movielens_to_postgres():
+    data = pd.read_csv('data/movielens.txt')
+    a = LabelEncoder().fit(data['movie_id'])
+    b = a.transform(data['movie_id'])
+    data['movie_id'] = b+1
+    # change data type
+
+    data = utils.convert_df_int64_to_int32(data)
+
+    user_data = data[['user_id', 'gender', 'age', 'occupation', 'zipcode']].copy()
+    movie_data = data[['movie_id', 'title', 'genres']].copy()
+    rating_data = data[['user_id', 'movie_id', 'timestamp', 'rating']].copy()
+
+    user_data.drop_duplicates(inplace=True)
+    user_data.reset_index(inplace=True, drop=True)
+    movie_data.drop_duplicates(inplace=True)
+    movie_data.reset_index(inplace=True, drop=True)
+    movie_data.sort_values(by='movie_id', inplace=True)
+    rating_data.drop_duplicates(inplace=True)
+    rating_data.reset_index(inplace=True, drop=True)
+    rating_data.sort_values(by='timestamp', inplace=True)
+
+    conn_string = utils.get_postgres_connection_config()
+    db = create_engine(conn_string)
+    conn = db.connect()
+
+    # load data to db
+    user_data.to_sql('movielens_user', db, index=False, if_exists='replace')
+    movie_data.to_sql('movielens_movie', db, index=False, if_exists='replace')
+    rating_data.to_sql('movielens_rating', db, index=False, if_exists='replace')
+
+    print("[INFO] load movielens to postgres success!")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Argument parser")
+
+    # Add arguments
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="all",
+        help="dataset name, available options: all, movielens",
+        required=False
+    )
+
+    args = parser.parse_args()
+    dataset = args.dataset
+
+    if dataset == 'all' or dataset == 'movielens':
+        load_movielens_to_postgres()
+
+
+if __name__ == "__main__":
+    main()
--- db-ml/baseline/utils.py
+++ db-ml/baseline/utils.py
@@ -1,12 +1,22 @@
 import time
+import numpy as np
 import os
+
 
 def mkdir(dir_path):
     if not os.path.isdir(dir_path):
         os.makedirs(dir_path)
 
+
 def get_postgres_connection_config():
     return "postgresql://postgresdb:postgresdb@localhost:5432/postgresdb"
+
+
+def convert_df_int64_to_int32(df):
+    int64_columns = df.select_dtypes(include=["int64"]).columns
+    df[int64_columns] = df[int64_columns].astype(np.int32)
+    return df
+
 
 class Timer(object):
     """A convenient class to measure the running time of a program"""
