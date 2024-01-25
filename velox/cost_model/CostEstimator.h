6b9bfacd7c29dc97a515cdd2bfc5d54f0d56277b
saifmasood <saiftraveller@gmail.com>
2024-01-25 02:31:46 +0000

    Added the cost_model folder

---
+++ velox/cost_model/CostEstimator.h
@@ -0,0 +1,72 @@
+#include "velox/cost_model/CostModel.h"
+
+using namespace facebook::velox;
+using namespace facebook::velox::test;
+using namespace facebook::velox::exec;
+using namespace facebook::velox::exec::test;
+using namespace facebook::velox::core;
+
+
+
+// perhaps only estimate cost for changes made and not the whole tree again
+// mcts can retrun the nodeId of the changes made (it makes one change only)
+// we need a way to get the next node for any node
+// 
+class CostEstimator {
+
+    protected:
+        std::unique_ptr<CostModel> costModel;
+
+    public:
+        virtual ~CostEstimator() = default;
+
+        // pass different cost models
+        // use a simple one for quick testing
+   
+        CostEstimator(std::unique_ptr<CostModel> model) : costModel(std::move(model)) {}
+
+        virtual CostEstimate estimateCost(std::shared_ptr<const core::PlanNode>& plan) const = 0; 
+};
+
+
+class SimpleCostEstimator : public CostEstimator {
+
+    public:
+
+        SimpleCostEstimator(std::unique_ptr<CostModel> model) : CostEstimator(std::move(model)) {}
+
+        CostEstimate estimateCost(std::shared_ptr<const core::PlanNode>& plan) const override {
+            if(!plan)
+                return CostEstimate(0,0,0);
+    
+            std::vector<Source> sources; 
+            // total cost of all the sources for the current node
+            // this will be added to the cost of the current node
+            // to get the total cost so far including the current node
+
+            // hashjoin
+            float srcCost = 0.0; // the current node
+            for (auto source : plan->sources()) {
+                    
+                CostEstimate estimate =  estimateCost(source);
+                
+                srcCost += estimate.cost;
+                std::shared_ptr<OutputStat> stat = std::make_shared<OutputStat>(OutputStat(estimate.outputRows , estimate.outputCols));
+                // inputs to the current node
+                // output from previous step is input to the current node
+                // hence the stat is the output stat of the sources
+                sources.push_back(Source(std::string(plan->name()), Source::Type::NODE, stat));
+            }
+           
+            CostEstimate estimate = costModel->getCost(plan, sources);
+            // total cost so far
+            estimate.cost += srcCost;
+            return estimate;
+    }
+
+};
+
+
+
+
+
---
+++ velox/cost_model/Source.h
@@ -0,0 +1,33 @@
+#include "velox/cost_model/Stat.h"
+
+class Source {
+
+    public:
+
+        enum Type {
+            NODE,
+            FILE,
+            VECTOR,
+            DATABASE
+        };
+
+        Source(std::string name = "", Type type = Type::NODE, std::shared_ptr<Stat> stats = nullptr) : name(name), type(type), stats(stats) {}
+
+        std::string getName(){
+            return name;
+        }
+
+        std::shared_ptr<Stat> getStats(){
+            return stats;
+        };
+
+        Type getType(){
+            return type;
+        }
+
+    private:
+        std::string name;
+        Type type;
+        std::shared_ptr<Stat> stats;
+       
+};
