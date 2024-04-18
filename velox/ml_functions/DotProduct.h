2426e8f1a2ac8a5c49cfb47b0efb218e8e2fac4d
Lixi Zhou <lixi.zhou@asu.edu>
2024-04-18 22:34:38 +0000

    implement several stuffs: 1. position encoding 2. dot product 3. huggingface serverless inference with velox

---
+++ velox/ml_functions/DotProduct.h
@@ -0,0 +1,82 @@
+#pragma once
+#include <Eigen/Dense>
+#include <cmath>
+#include <iostream>
+#include "functions.h"
+#include "velox/exec/tests/utils/AssertQueryBuilder.h"
+#include "velox/exec/tests/utils/PlanBuilder.h"
+#include "velox/exec/tests/utils/TempDirectoryPath.h"
+#include "velox/vector/tests/utils/VectorTestBase.h"
+
+using namespace facebook::velox;
+using namespace facebook::velox::test;
+using namespace facebook::velox::exec::test;
+using namespace facebook::velox::memory;
+
+class DotProduct : public exec::VectorFunction {
+ public:
+  DotProduct(int inputDims) {
+    inputDims_ = inputDims;
+  }
+
+  void apply(
+      const SelectivityVector& rows,
+      std::vector<VectorPtr>& args,
+      const TypePtr& type,
+      exec::EvalCtx& context,
+      VectorPtr& output) const override {
+    BaseVector::ensureWritable(rows, type, context.pool(), output);
+
+    // Decoder is required to handle address error, reference code:
+    // ArrayIntersectExcept.cpp
+    BaseVector* left = args[0].get();
+    BaseVector* right = args[1].get();
+
+    exec::LocalDecodedVector leftHolder(context, *left, rows);
+    auto decodedLeftArray = leftHolder.get();
+    auto baseLeftArray =
+        decodedLeftArray->base()->as<ArrayVector>()->elements();
+
+    exec::LocalDecodedVector rightHolder(context, *right, rows);
+    auto decodedRightArray = rightHolder.get();
+    auto baseRightArray = rightHolder->base()->as<ArrayVector>()->elements();
+
+    float* input1Values = baseLeftArray->values()->asMutable<float>();
+    float* input2Values = baseRightArray->values()->asMutable<float>();
+
+    auto numInput = rows.size();
+
+    Eigen::Map<
+        Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>
+        input1Matrix(input1Values, numInput, inputDims_);
+    Eigen::Map<
+        Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>
+        input2Matrix(input2Values, numInput, inputDims_);
+
+    std::vector<std::vector<float>> results;
+
+    for (int i = 0; i < rows.size(); i++) {
+      std::vector<float> r;
+      float dotProduct = input1Matrix.row(i).dot(input2Matrix.row(i));
+      r.push_back(dotProduct);
+      results.push_back(r);
+    }
+
+    VectorMaker maker{context.pool()};
+    output = maker.arrayVector<float>(results, REAL());
+  }
+
+  static std::vector<std::shared_ptr<exec::FunctionSignature>> signatures() {
+    return {exec::FunctionSignatureBuilder()
+                .argumentType("array(REAL)")
+                .argumentType("array(REAL)")
+                .returnType("array(REAL)")
+                .build()};
+  }
+  static std::string getName() {
+    return "dot_product";
+  }
+
+ private:
+  int inputDims_;
+};
