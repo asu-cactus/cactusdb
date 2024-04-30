4595bbf7b04881abe2461e68ea3a3c27bc227ff6
Lixi Zhou <lixi.zhou@asu.edu>
2024-04-30 05:12:44 +0000

    update decision forest model

--- velox/optimizer/DecisionForestUDF2RelationRewriteActionTest.cpp
+++ velox/optimizer/DecisionForestUDF2RelationRewriteActionTest.cpp
@@ -115,14 +115,14 @@ class DecisionForestUDF2RelationRewriteActionTest : public HiveConnectorTestBase
 
   void TestBody() override {}
 
-  void registerFunctions() {
+  void registerFunctions(std::string modelPath, int numCols) {
     std::cout << "To register function for TreePrediction" << std::endl;
 
     exec::registerVectorFunction(
         "decision_tree_predict",
         TreePrediction::signatures(),
         std::make_unique<TreePrediction>(
-            0, "resources/model/fraud_xgboost_10_8/0.txt", 28, false));
+            0, fmt::format("{}/0.txt", modelPath), numCols, false));
 
     std::cout << "To register type for Tree" << std::endl;
 
@@ -133,7 +133,7 @@ class DecisionForestUDF2RelationRewriteActionTest : public HiveConnectorTestBase
     exec::registerVectorFunction(
         "velox_decision_tree_predict",
         VeloxTreePrediction::signatures(),
-        std::make_unique<VeloxTreePrediction>(28));
+        std::make_unique<VeloxTreePrediction>(numCols));
 
     std::cout << "To register function for VeloxTreeConstruction" << std::endl;
 
@@ -148,7 +148,7 @@ class DecisionForestUDF2RelationRewriteActionTest : public HiveConnectorTestBase
         "decision_forest_predict",
         TreePrediction::signatures(),
         std::make_unique<ForestPrediction>(
-            "resources/model/fraud_xgboost_10_8", 28, true));
+            modelPath, numCols, true));
   }
 
   ArrayVectorPtr parseCSVFile(
@@ -335,18 +335,13 @@ class DecisionForestUDF2RelationRewriteActionTest : public HiveConnectorTestBase
     unregisterCustomType("tree_type");
   }
 
-  void testRewriteDecisionForestUDFPlan(bool rewrite) {
+  void testRewriteDecisionForestUDFPlan(std::string modelPath, std::string dataPath, bool rewrite, int numRows, int numCols) {
     // register functions and types that are needed for this test
-    registerFunctions();
+    registerFunctions(modelPath, numCols);
 
     // prepare features that are needed for this test
-    int numRows = 56962;
 
-    int numCols = 28;
-
-    std::string dataFilePath = "resources/data/creditcard_test.csv";
-
-    auto inputRowVector = loadData(dataFilePath, numRows, numCols);
+    auto inputRowVector = loadData(dataPath, numRows, numCols);
 
     // write the features to a file
 
@@ -399,43 +394,28 @@ class DecisionForestUDF2RelationRewriteActionTest : public HiveConnectorTestBase
   VectorMaker maker{pool_.get()};
 };
 
+DEFINE_string(model_path, "/home/velox/resources/model/fraud_xgboost_10_8", "Path to model");
+DEFINE_string(data_path, "/home/velox/resources/data/creditcard_test.csv", "Path to csv file");
+DEFINE_bool(rewrite, true, "Rewrite or not");
+DEFINE_int32(num_row, 56962,"Number of rows");
+DEFINE_int32(num_col, 28, "Number of columns");
+
 int main(int argc, char** argv) {
   folly::init(&argc, &argv, false);
   memory::MemoryManager::initialize({});
 
+  std::string modelPath = FLAGS_model_path;
+  std::string dataPath = FLAGS_data_path;
+  bool rewrite = FLAGS_rewrite;
+  int numRows = FLAGS_num_row;
+  int numCols = FLAGS_num_col;
+
   DecisionForestUDF2RelationRewriteActionTest demo;
 
-  bool rewrite = true;
+  std::cout << fmt::format("Model: {}, Data: {}, Rewrite: {}", modelPath, dataPath, rewrite) << std::endl;
 
-  if (argc > 1) {
-    if (strcmp(argv[1], "N") == 0) {
-      rewrite = false;
-    }
-  }
 
-  if (rewrite) {
-    std::cout
-        << "================= Run UDF-Centric Decision Forest w/ Rewriting ==================="
-        << std::endl
-        << std::endl;
 
-    demo.testRewriteDecisionForestUDFPlan(true);
+  demo.testRewriteDecisionForestUDFPlan(modelPath, dataPath, rewrite, numRows, numCols);
 
-  } else {
-    std::cout
-        << "================= Run UDF-Centric Decision Forest w/o Rewriting ==================="
-        << std::endl
-        << std::endl;
-
-    demo.testRewriteDecisionForestUDFPlan(false);
-  }
-
-  std::cout
-      << "--" << std::endl
-      << "[Usage] " << std::endl
-      << "./_build/release/velox/optimizer/rewrite_test Y  //run decision forest model with rewriting"
-      << std::endl
-      << "./_build/release/velox/optimizer/rewrite_test N  //run decision forest model with rewriting"
-      << std::endl
-      << "By default: Y is used" << std::endl;
 }
