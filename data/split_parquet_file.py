d8bf13857232d901b6bbd2c5fbcbb101b1545077
Lixi Zhou <lixi.zhou@asu.edu>
2024-08-01 22:23:22 +0000

    add util

--- data/split_parquet_file.py
+++ data/split_parquet_file.py
@@ -6,20 +6,24 @@ import os
 import math
 import shutil
 
+
 def remove_all_in_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
-        
+
         try:
             # Check if it's a file or directory and remove it
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.remove(file_path)
-                print(f'Removed file: {file_path}')
+                print(f"Removed file: {file_path}")
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
-                print(f'Removed directory and its contents: {file_path}')
+                print(f"Removed directory and its contents: {file_path}")
         except Exception as e:
-            print(f'Failed to remove {file_path}. Reason: {e}')
+            print(f"Failed to remove {file_path}. Reason: {e}")
 
 
 def write_orc(df, batch_size, dir_path):
@@ -29,17 +33,17 @@ def write_orc(df, batch_size, dir_path):
         df[start:end].to_parquet(path)
 
 
-remove_all_in_directory('movielens/rating')
-df1 = pq.read_table('movielens_rating_s_8192.parquet').to_pandas()
+remove_all_in_directory("movielens/rating")
+df1 = pq.read_table("movielens_rating_s_8192.parquet").to_pandas()
 batch_size = math.ceil(len(df1) / 8)
-write_orc(df1, batch_size, 'movielens/rating')
+write_orc(df1, batch_size, "movielens/rating")
 
-remove_all_in_directory('movielens/movie')
-df2 = pq.read_table('movielens_movie_s_8192.parquet').to_pandas()
+remove_all_in_directory("movielens/movie")
+df2 = pq.read_table("movielens_movie_s_8192.parquet").to_pandas()
 batch_size = math.ceil(len(df2) / 8)
-write_orc(df2, batch_size, 'movielens/movie')
+write_orc(df2, batch_size, "movielens/movie")
 
-remove_all_in_directory('movielens/user')
-df3 = pq.read_table('movielens_user_s_8192.parquet').to_pandas()
+remove_all_in_directory("movielens/user")
+df3 = pq.read_table("movielens_user_s_8192.parquet").to_pandas()
 batch_size = math.ceil(len(df3) / 8)
-write_orc(df3, batch_size, 'movielens/user')
+write_orc(df3, batch_size, "movielens/user")
