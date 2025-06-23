21c03ff03a21bd469304c5fd5faf14310eed4015
jrt1899 <jaytandel1899@gmail.com>
2025-06-23 17:26:56 +0000

    movielens qT4,qT9 done

--- velox/optimizer/tests/ProfileQueryGenerator.cpp
+++ velox/optimizer/tests/ProfileQueryGenerator.cpp
@@ -431,7 +431,7 @@ class IntegratedMCTSTest : public HiveConnectorTestBase {
               << myPlan.planNode()->toString(true, true) << std::endl;
     auto serializedPlan = myPlan.planNode()->serialize();
     std::string queryOutPutPath =
-        "/home/velox/velox/optimizer/tests/serializedQueryPlan.json";
+        "/home/cactusdb/velox/optimizer/tests/serializedQueryPlan.json";
     augmentSerializedPlan(serializedPlan, cataLog);
     writeStringToFile(folly::toJson(serializedPlan), queryOutPutPath);
 
@@ -445,7 +445,7 @@ class IntegratedMCTSTest : public HiveConnectorTestBase {
         pool_, numThreads, myPlan, cataLog, repeatRun, verbose);
 
     std::string latencyOutputPath =
-        "/home/velox/velox/optimizer/tests/executionLatency.txt";
+        "/home/cactusdb/velox/optimizer/tests/executionLatency.txt";
     writeStringToFile(std::to_string(executeTime), latencyOutputPath);
 
     std::cout << "[INFO] Execution time: " << executeTime << std::endl;
@@ -507,7 +507,7 @@ class IntegratedMCTSTest : public HiveConnectorTestBase {
     auto [minLatency, optimalRuleName] = minHeapOfRuleLatency.top();
 
     std::string optimalRuleOutputPath =
-        "/home/velox/velox/optimizer/tests/optimalRule.txt";
+        "/home/cactusdb/velox/optimizer/tests/optimalRule.txt";
     writeStringToFile(optimalRuleName, optimalRuleOutputPath);
 
     std::cout << "[INFO] Optimal Rule: " << optimalRuleName << std::endl;
@@ -548,18 +548,18 @@ class IntegratedMCTSTest : public HiveConnectorTestBase {
         pool_, numThreads, queryPlan, cataLog, repeatRun, verbose);
 
     std::string latencyOutputPath =
-        "/home/velox/velox/optimizer/tests/executionLatency.txt";
+        "/home/cactusdb/velox/optimizer/tests/executionLatency.txt";
     writeStringToFile(std::to_string(executeTime), latencyOutputPath);
 
     auto serializedPlan = queryPlan.planNode()->serialize();
     std::string queryOutPutPath =
-        "/home/velox/velox/optimizer/tests/serializedQueryPlan.json";
+        "/home/cactusdb/velox/optimizer/tests/serializedQueryPlan.json";
     augmentSerializedPlan(serializedPlan, cataLog);
     writeStringToFile(folly::toJson(serializedPlan), queryOutPutPath);
 
     auto queryPlanStr = queryPlan.planNode()->toString(true, true);
     std::string queryPlanStrOutputPath =
-        "/home/velox/velox/optimizer/tests/queryPlanStr.txt";
+        "/home/cactusdb/velox/optimizer/tests/queryPlanStr.txt";
     writeStringToFile(queryPlanStr, queryPlanStrOutputPath);
 
     std::cout << "[INFO] Execution time: " << executeTime << std::endl;
@@ -602,18 +602,18 @@ class IntegratedMCTSTest : public HiveConnectorTestBase {
 
               
     std::string latencyOutputPath =
-        "/home/velox/velox/optimizer/tests/executionLatency.txt";
+        "/home/cactusdb/velox/optimizer/tests/executionLatency.txt";
     writeStringToFile(std::to_string(executeTime), latencyOutputPath);
 
     auto serializedPlan = queryPlan.planNode()->serialize();
     std::string queryOutPutPath =
-        "/home/velox/velox/optimizer/tests/serializedQueryPlan.json";
+        "/home/cactusdb/velox/optimizer/tests/serializedQueryPlan.json";
     augmentSerializedPlan(serializedPlan, cataLog);
     writeStringToFile(folly::toJson(serializedPlan), queryOutPutPath);
 
     auto queryPlanStr = queryPlan.planNode()->toString(true, true);
     std::string queryPlanStrOutputPath =
-        "/home/velox/velox/optimizer/tests/queryPlanStr.txt";
+        "/home/cactusdb/velox/optimizer/tests/queryPlanStr.txt";
     writeStringToFile(queryPlanStr, queryPlanStrOutputPath);
 
     std::cout << "[INFO] Execution time: " << executeTime << std::endl;
@@ -724,7 +724,7 @@ int main(int argc, char** argv) {
     dummyFeatureSizes.push_back(movieFeatureSize);
     std::cout << "numberOfTuples: " << numberOfTuples << std::endl;
     std::cout << "dummyFeatureSizes: " << dummyFeatureSizes << std::endl;
-
+    std::cout << "Let's begin!!!";
     demo.benchmarkQueryFromTemplate1(
         workload,
         queryTemplate,
